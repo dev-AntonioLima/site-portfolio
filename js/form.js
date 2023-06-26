ce523315d9fd9aaad45781b2d9ac43547c509b4b
Antonio Lima <antonio55senac@gmail.com>
2023-06-26 00:02:08 +0000

    Novo Modelo

--- js/form.js
+++
@@ -1,9 +0,0 @@
-function Enviar() {
-
-    var nome = document.getElementById("nomeid");
-
-    if (nome.value != "") {
-        alert('Obrigado  ' +nome.value  + '  sua mensagem foi enviada com sucesso :)');
-    }
-
-}
