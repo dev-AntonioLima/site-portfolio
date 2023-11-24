1bdd60aae028eab31e59355d90c12d26c84011b5
Antonio Lima <antonio55senac@gmail.com>
2023-11-24 17:43:28 +0000

    efeito type rj

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
-function typeWriter(el) {
+function typeWriter(el, elementor) {
     const textArray = el.innerHTML.split('');
     el.innerHTML = '';
 
     textArray.forEach((letter, i) => {
-        setTimeout (() => (el.innerHTML += letter), 90 * i)});
+        setTimeout(() => (el.innerHTML += letter), 90 * i)
+        console.log(el.lenght);
+    });
+
+
+    if (el.length = 23) {
+        const textArray = elementor.innerHTML.split('');
+        elementor.innerHTML = '';
+
+        textArray.forEach((letter, i) => {
+            setTimeout(() => (elementor.innerHTML += letter), 90 * i)
+        });
 
         console.log("teste");
+    }
 }
 
-typeWriter(elementEl);
+typeWriter(elementEl, elementEm);
